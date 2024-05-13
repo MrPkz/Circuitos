4f59e34e74088b0f931280ef2978046874eb33e6
RamiroMadera <ramiro.madera@alu.ing.unlp.edu.ar>
2024-05-13 12:52:17 -0300

    Correccion en timer.h

--- 2/Microchip2/Microchip2/timer.h
+++ 2/Microchip2/Microchip2/timer.h
@@ -12,6 +12,6 @@
 
 void Iniciar_Timer();
 void printTime(uint32_t t);
-void getTime(uint8_t m1, uint8_t m2, uint8_t s1, uint8_t s2, uint8_t ms1, uint8_t ms2, uint8_t ms3);
+void getTime(uint32_t t, uint8_t *m1, uint8_t *m2, uint8_t *s1, uint8_t *s2, uint8_t *ms1, uint8_t *ms2, uint8_t *ms3);
 
 #endif /* TIMER_H_ */
